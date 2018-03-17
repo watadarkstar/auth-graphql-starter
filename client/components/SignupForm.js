0f15ffe5a548a8439bde39b43affe79870cebf9d
Adrian Carolli <adrian.carolli1@gmail.com>
2018-03-17 11:02:15 -0400

    Finished course

---
+++ client/components/SignupForm.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import AuthForm from './AuthForm';
+import { graphql } from 'react-apollo';
+import mutation from '../mutations/Signup';
+import query from '../queries/CurrentUser';
+import { hashHistory } from 'react-router';
+
+class SignupForm extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { errors: [] };
+  }
+
+  componentWillUpdate(nextProps) {
+    if (!this.props.data.user && nextProps.data.user) {
+      // redirect to dashboard
+      hashHistory.push('/dashboard');
+    }
+  }
+
+  onSubmit({ email, password }) {
+    this.props.mutate({
+      variables: { email, password },
+      refetchQueries: [{ query }]
+    }).catch(res => {
+      const errors = res.graphQLErrors.map((err) => err.message);
+      this.setState({ errors });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Sign Up</h3>
+        <AuthForm onSubmit={this.onSubmit.bind(this)} errors={this.state.errors} />
+      </div>
+    );
+  }
+}
+
+export default graphql(query)(
+  graphql(mutation)(SignupForm)
+);
--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://admin:alecia12@ds215709.mlab.com:15709/auth';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
